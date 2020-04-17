ffc07dfb5c6aab59ee820c3b012b6613af3b48ab
BeYourself <15262460969@163.com>
2020-04-17 16:43:51 +0800

    第二次增加配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,39 @@
 const path = require('path');
 
-console.log("=========打印配置==========");
-console.log(path);
-console.log(__filename);
-console.log(__dirname);
-console.log("========打印配置完毕===========");
+// console.log("=========打印配置==========");
+// console.log(path);
+// console.log(__filename);
+// console.log(__dirname);
+// console.log("========打印配置完毕===========");
 
 module.exports = {
-    entry: './src/index.js',
+    // entry: './src/index.js',单个入口文件
+
+    // 多个入口文件
+    entry:{
+        app:"./src/index.js",
+        print:"./src/print.js"
+    },
     output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
+    //   filename: 'bundle.js',
+        filename:"[name].bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                use:[
+                    "file-loader"
+                ]
+            }
+        ]
     }
   };
